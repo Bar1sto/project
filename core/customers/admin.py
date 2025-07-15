bc54df670372d2a058b86c3a4a0feab9e31b2a7c
Bar <vovad3039@gmail.com>
2025-07-15 17:51:23 +0300

    начало админ панели, начал с Клиента 15.07.25

--- core/customers/admin.py
+++ core/customers/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from orders.models import Order
 from .models import (
     Client,
     Bonus,
@@ -7,11 +8,63 @@ from .models import (
     PromocodeUsage
     )
 
+
+class OrderInline(admin.StackedInline):
+    model = Order
+    extra = 0
+    readonly_fields = ('order_total', 'create_at', 'status')
+    can_delete = False
+    list_filter = ('status',)
+    show_change_link = True
+    
+    def get_fields(self, request, obj=None):
+        return ('id', 'order_total', 'status')
+
+
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    model = Client
+    list_display = (
+        'name',
+        'surname',
+        'email',
+        )
     
-
+    fieldsets = (
+        (
+            'Основная информация', {
+                'fields': (
+                    'surname',
+                    'name',
+                    'patronymic',
+                )
+            }
+        ),
+        (
+            'Персональные данные', {
+                'fields': (
+                    'phone_number',
+                    'email',
+                    'birthday',
+                )
+            }
+        ),
+        (
+            'Фотография', {
+                'fields': (
+                    'image',
+                )
+            }
+        )
+    )
+    
+    search_fields = [
+        'surname',
+        'phone_number',
+        'email',
+    ]
+    
+    inlines = [OrderInline]
+    
 
 @admin.register(Bonus)
 class BonusAdmin(admin.ModelAdmin):
